5e059fc80f7f5c643708ee4bdc4a5b00b15a40fa
lailaBouadif <bouadif.laila@gmail.com>
2022-08-31 12:09:37 +0100

    initial commit

---
+++ src/main/java/com/valueit/systemegestioncommande/SystemeGestionCommandeApplication.java
@@ -0,0 +1,13 @@
+package com.valueit.systemegestioncommande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeGestionCommandeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemeGestionCommandeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/valueit/systemegestioncommande/SystemeGestionCommandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valueit.systemegestioncommande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeGestionCommandeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
